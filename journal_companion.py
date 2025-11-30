9fbb0c3ad4792c51424b53dcdd22d84333602f4f
Miguel Pena <mgiuel.n.pena@gmail.com>
2025-11-29 22:23:52 -0500

    new adjustment - 11/29

---
+++ journal_companion.py
@@ -0,0 +1,13 @@
+import justice_navigator_info
+import pandas as pd
+import numpy as np
+import os
+
+autoreset=True
+
+print(input(f"Welcome to your Journal Companion, how can I address you: "))
+
+# def main_menu():
+#     # counter for invalid option in dashboard.
+#     invalid_count = 0
+
